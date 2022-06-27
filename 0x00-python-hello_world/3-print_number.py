f2f9b7d9d10fc38decbedaa73737b4b5820b29c8
Shumba250 <mudakikwaaimable05@gmail.com>
2022-06-28 00:45:53 +0200

    fixed

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3
-number = 98
-print (f"{number} Battery street")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + ' ' + str[107:111] + ' ' + str[:6]
+print(str)
