7f7324df79a6b35ffc0181a7714e8aba5226b991
Shumba250 <mudakikwaaimable05@gmail.com>
2022-06-30 13:36:51 +0200

    fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-    args = len(sys.argv) - 1
-    if args != 3:
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    opr = sys.argv[2]
-    if opr != '+' and opr != '-' and opr != '*' and opr != '/':
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if opr == '+':
+    if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif opr == '-':
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif opr == '*':
+    elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    res = 0
+    result = 0
     for arg in sys.argv:
         if arg != sys.argv[0]:
-            res += int(arg)
-    print(res)
+            result += int(arg)
+    print(result)
